b2d878d46595b4e89b05ffb6f1e9a80e2b38d560
konstantin <kotmix@bk.ru>
2020-11-03 21:46:55 +0400

    4.5. Отладка программы в IDEA [#401083]

--- test/ru/job4j/calculator/SqAreaTest.java
+++ test/ru/job4j/calculator/SqAreaTest.java
@@ -1,10 +1,29 @@
 package ru.job4j.calculator;
 
 import org.junit.Test;
-import org.junit.Assert;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 import ru.job4j.calculator.SqArea;
 
 public class SqAreaTest {
+
+    @Test
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        int expected = 1;
+        int s = (int) SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public void whenp6k2s2() {
+        int k = 2;
+        int p = 6;
+        int expected = 2;
+        int s = (int) SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
 
     @Test
     public void square() {
@@ -12,6 +31,6 @@ public class SqAreaTest {
         int p = 6;
         int k = 2;
         double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        assertEquals(expected, out, 0.01);
     }
 }
