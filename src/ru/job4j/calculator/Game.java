be18e968bbe43f2955877493e2a8e151986c45a5
konstantin <kotmix@bk.ru>
2020-11-03 22:37:37 +0400

    5. Стоит ли играть?

---
+++ src/ru/job4j/calculator/Game.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculator;
+
+public class Game {
+    public static int checkGame(double percent, int prize, int pay) {
+        double result = 0;
+        if (percent * prize > pay) {
+            result =  percent * prize - pay;
+        } else if ((percent * prize - pay) <= 0) {
+            result = 0;
+        }
+        return (int) result;
+    }
+}
