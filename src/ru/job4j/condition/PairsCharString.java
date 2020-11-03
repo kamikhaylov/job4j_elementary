c7f9157588527f5d1381e88e0ece7e826ff07973
konstantin <kotmix@bk.ru>
2020-11-04 01:31:36 +0400

    12. Необычные пары

---
+++ src/ru/job4j/condition/PairsCharString.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class PairsCharString {
+    public static boolean check(String l, String r) {
+        if ((l.length() == 0) && (r.length() == 0)) {
+            return true;
+        }
+        char oneL = l.charAt(0);
+        char oneR = r.charAt(0);
+        char lastL = l.charAt(l.length() - 1);
+        char lastR = r.charAt(r.length() - 1);
+        boolean result = false;
+
+        if ((oneL == lastR) && (oneR == lastL)) {
+            result = true;
+        }
+        return result;
+    }
+}
