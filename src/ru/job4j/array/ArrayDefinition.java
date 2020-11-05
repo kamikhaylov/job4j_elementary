690de569cebc3fc320d1c6435cf021c422b88e6a
konstantin <kotmix@bk.ru>
2020-11-05 22:11:13 +0400

    6.0.2. Заполнение массива. [#401118]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,19 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
 
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        names[0] = "Konstantin";
+        names[1] = "Ivan";
+        names[2] = "Roman";
+        names[3] = "Sergei";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
