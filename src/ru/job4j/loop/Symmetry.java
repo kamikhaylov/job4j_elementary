e99e2d3fb8c34a2085fde6f990a50d566def23e9
konstantin <kotmix@bk.ru>
2020-11-05 20:48:18 +0400

    12. Симметричное число

---
+++ src/ru/job4j/loop/Symmetry.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Symmetry {
+    public static boolean check(int i) {
+        StringBuilder sb = new StringBuilder(String.valueOf(i));
+        int k = sb.length() - 1;
+        for (int j = 0; j < sb.length() / 2; j++) {
+            if (sb.charAt(j) != sb.charAt(k)) {
+                return false;
+            }
+            k--;
+        }
+        return true;
+    }
+}
