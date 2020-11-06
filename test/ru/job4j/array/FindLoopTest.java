69959c82a45bcfc123b317a5edc7e09a3e2d59d2
konstantin <kotmix@bk.ru>
2020-11-06 11:20:35 +0400

    6.5. Поиск индекса в диапазоне. [#401111]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -8,8 +8,8 @@ import static org.junit.Assert.*;
 public class FindLoopTest {
 
     @Test
-    public void whenArrayHas5Then0() {
-        int[] input = new int[] {5, 4, 3, 2};
+    public void whenArrayHasLength5Then0() {
+        int[] input = new int[] {5, 10, 3};
         int value = 5;
         int result = FindLoop.indexOf(input, value);
         int expect = 0;
@@ -21,6 +21,28 @@ public class FindLoopTest {
         int[] input = new int[] {5, 4, 3, 2, 1, 10};
         int value = 7;
         int result = FindLoop.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind2() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindNot() {
+        int[] input = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int value = 7;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = -1;
         assertThat(result, is(expect));
     }
