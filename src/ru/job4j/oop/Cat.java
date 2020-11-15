d48b6550564d482fed10ce3d016c0eeb4fff53ee
konstantin <kotmix@bk.ru>
2020-11-15 14:01:32 +0400

    1.5. Поля объекта [#401298]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -1,8 +1,30 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String name;
+    private String food;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println("Кот по имени " + this.name + " съел " + this.food);
+    }
+
     public static void main(String[] args) {
-            Cat peppy = new Cat();
-            Cat sparky = new Cat();
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
