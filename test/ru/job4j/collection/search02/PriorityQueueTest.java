eec21b35bc25a3d4b5480566af54fed7cd059b1c
konstantin <kotmix@bk.ru>
2020-12-02 22:41:06 +0400

    2. Очередь с приоритетом на LinkedList [#401226]

---
+++ test/ru/job4j/collection/search02/PriorityQueueTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection.search02;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PriorityQueueTest {
+    @Test
+    public void whenHigherPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+
+    @Test
+    public void whenHigherPriorityTwo() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 7));
+        queue.put(new Task("middle", 2));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("middle"));
+        result = queue.take();
+        assertThat(result.getDesc(), is("low"));
+    }
+}
