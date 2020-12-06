f93c8308efb403caef2c9d227671a7bc6cc0e0d1
konstantin <kotmix@bk.ru>
2020-12-06 20:32:20 +0400

    1. Организовать сортировку User [#401221]

---
+++ src/ru/job4j/collection/user/SortByNameUser.java
@@ -0,0 +1,10 @@
+package ru.job4j.collection.user;
+
+import java.util.Comparator;
+
+public class SortByNameUser implements Comparator<User> {
+    @Override
+    public int compare(User first, User second) {
+        return first.getName().compareTo(second.getName());
+    }
+}
---
+++ test/ru/job4j/collection/user/UserTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection.user;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UserTest {
+    @Test
+    public void whenAsc() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Petr", 32));
+        users.add(new User("Ivan", 31));
+        Iterator<User> it = users.iterator();
+        assertThat(it.next(), is(new User("Ivan", 31)));
+        assertThat(it.next(), is(new User("Petr", 32)));
+    }
+
+    @Test
+    public void whenAscTheSameNames() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Ivan", 32));
+        users.add(new User("Petr", 30));
+        users.add(new User("Ivan", 31));
+        Iterator<User> it = users.iterator();
+        assertThat(it.next(), is(new User("Ivan", 31)));
+        assertThat(it.next(), is(new User("Ivan", 32)));
+        assertThat(it.next(), is(new User("Petr", 30)));
+    }
+
+    @Test
+    public void whenComparePertVSIvan() {
+        int rsl = new User("Petr", 32)
+                .compareTo(
+                        new User("Ivan", 31)
+                );
+        assertThat(rsl, greaterThan(0));
+    }
+}
