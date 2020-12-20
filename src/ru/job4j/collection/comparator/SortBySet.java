c25496e23b62cee961ed0fa4280b3d9c2f70b92c
konstantin <kotmix@bk.ru>
2020-12-20 21:49:40 +0400

    2.5. Способы сортировки. Сортировка Collection. Сортировка через создание новой коллекции

---
+++ src/ru/job4j/collection/comparator/SortBySet.java
@@ -0,0 +1,10 @@
+package ru.job4j.collection.comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SortBySet {
+    public static Set<String> sort(List<String> list) {
+        return new TreeSet<>(list);
+    }
+}
