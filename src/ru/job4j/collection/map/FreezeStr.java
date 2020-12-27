4b81011134d8506fc24e94b3ca1a2b3981138ded
konstantin <kotmix@bk.ru>
2020-12-27 20:53:06 +0400

    2. Проверить две строчки на идентичность. Использование HashMap

--- src/ru/job4j/collection/map/FreezeStr.java
+++ src/ru/job4j/collection/map/FreezeStr.java
@@ -1,13 +1,24 @@
 package ru.job4j.collection.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        if (left.length() != right.length()) {
-            return false;
+        Map<Character, Integer> leftMap = createCharMap(left);
+        Map<Character, Integer> rightMap = createCharMap(right);
+        return leftMap.equals(rightMap);
+    }
+
+    public static Map<Character, Integer> createCharMap(String str) {
+        Map<Character, Integer> charMap = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            if (charMap.containsKey(c)) {
+                charMap.put(c, charMap.get(c) + 1);
+            } else {
+                charMap.put(c, 1);
+            }
         }
-        for (String s : left.split("")) {
-            right = right.replaceFirst(s, "");
-        }
-        return right.length() == 0;
+        return charMap;
     }
 }
