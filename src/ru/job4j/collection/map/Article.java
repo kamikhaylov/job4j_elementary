ae2cf41a23a3b96c2bdbc53c6dcee3cbf50dc6f3
konstantin <kotmix@bk.ru>
2020-12-27 19:50:09 +0400

    1. Генератор текста. Использование HashSet

--- src/ru/job4j/collection/map/Article.java
+++ src/ru/job4j/collection/map/Article.java
@@ -1,12 +1,17 @@
 package ru.job4j.collection.map;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        for (String s : line.split(" ")) {
-            if (!origin.contains(s)) {
-                return false;
-            }
+        Set<String> setOrigin = new HashSet<>(Arrays.asList(origin.split("[^a-zA-Zа-яА-Я]")));
+        Set<String> setLine = new HashSet<>(Arrays.asList(line.split("[^a-zA-Zа-яА-Я]")));
+        int sizeOrirgin = setOrigin.size();
+        for (String s : setLine) {
+            setOrigin.add(s);
         }
-        return true;
+        return sizeOrirgin == setOrigin.size();
     }
 }
