b642bc07fba7338d54eda2e87a0ba69a4a71b917
konstantin <kotmix@bk.ru>
2021-01-10 23:42:04 +0400

    0. Stream API [#401057]

---
+++ src/ru/job4j/collection/stream/StreamInt.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamInt {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(-1);
+        numbers.add(1);
+        numbers.add(-2);
+        numbers.add(2);
+        numbers.add(-3);
+        numbers.add(3);
+        List<Integer> numbersPositive = numbers
+                .stream()
+                .filter(number -> number > 0)
+                .collect(Collectors.toList());
+        numbersPositive.forEach(System.out::println);
+    }
+}
