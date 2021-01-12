0b07bead21ce9aa85e111e540baa1f191e32e617
konstantin <kotmix@bk.ru>
2021-01-12 22:00:22 +0400

    2. Список адресов [#401059]

---
+++ src/ru/job4j/collection/stream/travelCompany/Profile.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection.stream.travelCompany;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profile {
+    private Address address;
+
+    public Profile() {
+    }
+
+    public Profile(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .collect(Collectors.toList());
+    }
+}
