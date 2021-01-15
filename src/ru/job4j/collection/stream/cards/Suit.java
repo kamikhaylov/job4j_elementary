529300999bcff847a284dc1ae567cc59de5c79b1
konstantin <kotmix@bk.ru>
2021-01-15 09:46:58 +0400

    8. Перенос методов в Stream API [#401065]

---
+++ src/ru/job4j/collection/stream/cards/Suit.java
@@ -0,0 +1,8 @@
+package ru.job4j.collection.stream.cards;
+
+public enum Suit {
+    Diamonds,
+    Hearts,
+    Spades,
+    Clubs
+}
---
+++ src/ru/job4j/collection/stream/cards/Value.java
@@ -0,0 +1,7 @@
+package ru.job4j.collection.stream.cards;
+
+public enum Value {
+    V_6,
+    V_7,
+    V_8
+}
