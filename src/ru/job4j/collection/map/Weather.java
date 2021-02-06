57e0f4f1d30f3c1967966f82f82b727df50bc2ab
konstantin <kotmix@bk.ru>
2021-02-06 11:56:44 +0400

    13. Данные об осадках

---
+++ src/ru/job4j/collection/map/Weather.java
@@ -0,0 +1,55 @@
+package ru.job4j.collection.map;
+
+import java.util.*;
+
+public class Weather {
+    public static List<Info> editData(List<Info> list) {
+        List<Info> rsl = new ArrayList<>();
+        Map<String, Integer> city = new HashMap<>();
+        for (Info info : list) {
+            city.computeIfPresent(info.getCity(), (k, v) -> v + info.getRainfall());
+            city.putIfAbsent(info.getCity(), info.getRainfall());
+        }
+        for (String c : city.keySet()) {
+            rsl.add(new Info(c, city.get(c)));
+        }
+        return rsl;
+    }
+
+    public static class Info {
+        private String city;
+
+        private int rainfall;
+
+        public Info(String city, int rainfall) {
+            this.city = city;
+            this.rainfall = rainfall;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public int getRainfall() {
+            return rainfall;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Info info = (Info) o;
+            return rainfall == info.rainfall &&
+                    Objects.equals(city, info.city);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(city, rainfall);
+        }
+    }
+}
